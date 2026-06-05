b64edbb4e3efe11817449e5478896f353fdfb1ab
Pablo Siklosi <pablosiklosi@gmail.com>
2026-06-05 17:14:14 -0300

    chore(math-visuals): export MathWatermark from index and re-export from math/MathThemePlate

--- src/components/math-visuals/index.ts
+++ src/components/math-visuals/index.ts
@@ -1,4 +1,6 @@
 export { MathThemePlate } from "./MathThemePlate";
 export type { MathTheme, MathThemePlateProps, MathThemeVariant } from "./MathThemePlate";
+export { MathWatermark } from "./MathWatermark";
+export type { MathWatermarkProps } from "./MathWatermark";
 export { mathThemeForSkill, mathThemeForTopic } from "./topic-map";
 export { MATH_THEMES } from "./types";
